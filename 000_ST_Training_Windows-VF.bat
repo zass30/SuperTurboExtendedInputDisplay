187ded56e440d47c1d7726a1890c37e888b86b40
zass30 <jbeasley@gmail.com>
2020-09-03 10:45:42 -0700

    Delete 000_ST_Training_Windows-VF.bat

--- 000_ST_Training_Windows-VF.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo STARTING Training Mode for Super Street Fighter 2X: Grand Master challenge
-echo Make sure you have ssf2.zip ssf2t.zip ssf2xj.zip in the 'roms' folder
-mame-rr.exe -noreadconfig -rompath roms -artpath artwork -cheatpath cheat -snapshot_directory snap -diff_directory diff -cfg_directory cfg -inipath ini -nvram_directory nvram -input_directory inp -state_directory sta -skip_gameinfo -cheat -pause_brightness 1 -antialias -filter -lua "sf2-hitboxes_and_VF_input.lua" -maximize -window -nokeepaspect -resolution 990x723@60 ssf2xj
-pause
